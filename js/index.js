e77d02b6badddf69d1aa1b8dd62ebb10a3872337
Rodrigo <rodrigo.ezequiel.ruiz@gmail.com>
2022-09-19 05:26:03 -0300

    identado index.js

--- js/index.js
+++ js/index.js
@@ -9,9 +9,10 @@ const inyeccionJS = document.querySelector('#inyeccionJS');
 const mensaje = document.createElement("p");
 const precioTotal = document.getElementById("precioTotal")
 
+//Función que actualiza el carrito
 const actualizarCarrete = () => {
     mensajeHTML.innerHTML = ""
-    carrete.forEach((libro) =>{
+    carrete.forEach((libro) => {
         const div = document.createElement("div")
         div.className = ("productoEnCarrete")
         div.innerHTML = `<p> ${libro.titulo} </p>
@@ -20,20 +21,20 @@ const actualizarCarrete = () => {
         mensajeHTML.appendChild(div)
     })
     precioTotal.innerText = carrete.reduce((acc, prod) => acc + prod.precio, 0)
- }
+}
 
 let carrete
-if(localStorage.getItem('carrete')) {
-   carrete = JSON.parse(localStorage.getItem('carrete'))
-   actualizarCarrete()  
+if (localStorage.getItem('carrete')) {
+    carrete = JSON.parse(localStorage.getItem('carrete'))
+    actualizarCarrete()
 
 } else {
-   carrete = []
-   document.querySelector("p").remove()
+    carrete = []
+    document.querySelector("p").remove()
 
-   mensaje.innerHTML = ` <strong>El carrito está vacio.</strong> `
+    mensaje.innerHTML = ` <strong>El carrito está vacio.</strong> `
 
-mensajeHTML.append(mensaje)
+    mensajeHTML.append(mensaje)
 }
 
 
@@ -80,21 +81,19 @@ function renderizarInfoLibros(e) {
     console.log(carrete);
 }
 
- 
-const realizarCompraCarrete = () =>{
+
+const realizarCompraCarrete = () => {
     if (carrete.length === 0) {
-      
-       document.querySelector("p").remove()
-     
-       mensaje.innerHTML = ` <strong>El carrito está vacio.</strong> `
-    
-    mensajeHTML.append(mensaje)
-     
-      } else {
 
-        if (confirm("¿Estás segur@ de realizar la compra?")){
+        document.querySelector("p").remove()
 
-           /*  document.querySelector("p").remove() */
+        mensaje.innerHTML = ` <strong>El carrito está vacio.</strong> `
+
+        mensajeHTML.append(mensaje)
+
+    } else {
+
+        if (confirm("¿Estás segur@ de realizar la compra?")) {
 
             carrete = []
 
@@ -104,53 +103,40 @@ const realizarCompraCarrete = () =>{
                 'Tu compra se realizó con éxito!',
                 'Pronto te enviaremos la información de tu factura',
                 'success'
-              )
+            )
+            actualizarCarrete()
 
-         /*  mensaje.innerHTML = ` <strong>¡Gracias por tu compra! Tu carrito se vació.</strong>  ` 
-
-       mensajeHTML.append(mensaje) */
-       actualizarCarrete()
-       
         }
-      }
+    }
 }
 
 const vaciarCarrete = () => {
     mensajeHTML.innerHTML = ""
     localStorage.clear()
     carrete = []
-  document.querySelector("p").remove()
-  mensaje.innerHTML = `<strong>El carrito se vació.</strong>`
-mensajeHTML.append(mensaje)
-actualizarCarrete()
+    document.querySelector("p").remove()
+    mensaje.innerHTML = `<strong>El carrito se vació.</strong>`
+    mensajeHTML.append(mensaje)
+    actualizarCarrete()
 
 }
 
 
-/* const eliminarDelCarrete = (libroAEliminar) => {
-    const item = carrete.find((libro) => libro.id == libroAEliminar)
-    const indice = carrete.indexOf(item)
-    carrete.splice(indice, 1)
-    actualizarCarrete()
-} */
-
 //Add Event Listeners
-//Selectores
 
-
- botonComprar.addEventListener('click', realizarCompraCarrete) 
+botonComprar.addEventListener('click', realizarCompraCarrete)
 botonEliminar.addEventListener('click', vaciarCarrete)
 
 //Comunicación con Data.JSON 
 fetch('data.json')
 
-.then((response) => response.json())
+    .then((response) => response.json())
 
-.then((data) => {
-   librosDisponibles = data
-    renderizarLibrosDisponibles()
-})
+    .then((data) => {
+        librosDisponibles = data
+        renderizarLibrosDisponibles()
+    })
 
-.catch((error) => {
-   console.log(error)
-})
+    .catch((error) => {
+        console.log(error)
+    })
